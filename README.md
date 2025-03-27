9a9acc7ef3682399f66f57521d9c7d0ed680a127
jay menorca <zaimenorca@gmail.com>
2025-03-27 11:51:19 -0400

    updated CustomerOrderRepository to add method for custom query: @Query, getProductsOrderedByCustomerName(name):Flux<Product>

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Jay's project/practice repo for Reactive Microservices : WebFlux + Project Reactor
 
 proj: webflux-sandbox
-- Created CustomerOrder entity: used UUID for Id, [BP] java.time.Instant for orderDate (timestamp); created <<CustomerIrderRepository>>; refactored repositories to add @Repository
+- [BP] updated CustomerOrderRepository to add method for custom query: @Query, getProductsOrderedByCustomerName(name):Flux<Product>
+- Created CustomerOrder entity: used UUID for Id, [BP] java.time.Instant for orderDate (timestamp); created <<CustomerOrderRepository>>; refactored repositories to add @Repository
 - Pagination in r2dbc: modified ProductRepository, added findAllBy(Pageable):Flux<Product>; added test findPageableAllBy - test to return page 1 with 3 items per pg: used PageRequest, Sort.by("price"), ascending()
 - done Assignment25: create <<ProductRepository>>, add findByPriceBetween(), create ProductRepositoryTest, create testFindByPriceBetween()
 - tested setting logging level on app.yml and via @SpringBootTest property
--- webflux-sandbox/src/main/java/jayslabs/demo03/repository/CustomerOrderRepository.java
+++ webflux-sandbox/src/main/java/jayslabs/demo03/repository/CustomerOrderRepository.java
@@ -2,15 +2,26 @@ package jayslabs.demo03.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import jayslabs.demo03.entity.CustomerOrder;
+import jayslabs.demo03.entity.Product;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface CustomerOrderRepository extends ReactiveCrudRepository<CustomerOrder, UUID> {
 
-    // @Query("SELECT p.* FROM customer c INNER JOIN customer_order co ON c.id = co.customer_id INNER JOIN product p ON co.product_id = p.id 
-    // WHERE c.name = :name")
+    //find all the products ordered by the given customer name
+    // @Query("SELECT co.* FROM customer c INNER JOIN customer_order co ON c.id = co.customer_id INNER JOIN product p ON co.product_id = p.id WHERE c.name = :name")
     // Flux<CustomerOrder> findAllByCustomerName(String name);
+
+    @Query("""
+        SELECT p.* FROM customer c 
+        INNER JOIN customer_order co ON c.id = co.customer_id 
+        INNER JOIN product p ON co.product_id = p.id 
+        WHERE c.name = :name
+        """)
+    Flux<Product> getProductsOrderedByCustomerName(String name);
 }
